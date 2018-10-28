60aee946d672525d036bed429f5fffb3d79a9951
rn1k <maki_r12@yahoo.co.jp>
2018-10-29 00:59:27 +0900

    イベントハンドラ追加

--- src/App.js
+++ src/App.js
@@ -4,16 +4,43 @@ import TodoList from './TodoList'
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      tasks: [
+          {title: 'Todo1つ目', id: 0},
+          {title: 'Todo2つ目', id: 1}
+      ],
+      uniqueId: 2
+    }
+    this.addTodo = this.addTodo.bind(this)
+    this.resetTodo = this.resetTodo.bind(this)
+  }
+  addTodo(title) {
+    const {
+      tasks, uniqueId
+    } = this.state
+    tasks.push({
+      title,
+      id: uniqueId
+    })
+    this.setState({
+      tasks,
+      uniqueId: uniqueId + 1
+    })
+  }
+  resetTodo() {
+    this.setState({
+      tasks: []
+    })
+  }
   render() {
-    const tasks = [
-      {title: 'Todo1つ目', id: 0},
-      {title: 'Todo2つ目', id: 1}
-    ]
     return (
       <div>
         <h1>TODO App</h1>
-        <TodoInput />
-        <TodoList tasks={tasks} />
+        <button onClick={this.resetTodo}>リセット</button>
+        <TodoInput addTodo={this.addTodo} />
+        <TodoList tasks={this.state.tasks} />
       </div>
     );
   }
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,11 +3,27 @@ import React, { Component } from "react";
 
 
 class TodoInput extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            inputValue: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleChange(e) {
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+    handleClick() {
+        this.props.addTodo(this.state.inputValue)
+    }
     render () {
         return (
             <div>
-                <input type="text" />
-                <button>登録</button>
+                <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
+                <button onClick={this.handleClick}>登録</button>
             </div>
         )
     }
