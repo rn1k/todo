49b6bc10c75da2e412ec26ffa38cf8e2b10bf033
rn1k <maki_r12@yahoo.co.jp>
2018-10-29 00:40:05 +0900

    stateなし・UIだけ

---
+++ src/TodoInput.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from "react";
+
+
+class TodoInput extends Component {
+    render () {
+        return (
+            <div>
+                <input type="text" />
+                <button>登録</button>
+            </div>
+        )
+    }
+}
+
+export default TodoInput
